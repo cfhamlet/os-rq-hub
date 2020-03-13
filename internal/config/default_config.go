bd79f080c50de0ae0696231af595ec2b8a9c25c8
Ozzy <cfhamlet@gmail.com>
2020-03-13 21:36:46 +0800

    add: update configure

--- internal/config/default_config.go
+++ internal/config/default_config.go
@@ -5,13 +5,14 @@ const EnvPrefix = "RQ_HUB"
 
 // DefaultConfig TODO
 var DefaultConfig = map[string]interface{}{
-	"debug":               false,
-	"log.level":           "info",
-	"http.addr":           ":6788",
-	"http.log.enable":     true,
-	"http.api.prefix":     "",
-	"redis.addr":          "localhost:6379",
-	"redis.password":      "",
-	"redis.db":            0,
-	"redis.max_mem_limit": 256 << 20,
+	"debug":              false,
+	"log.level":          "info",
+	"http.addr":          ":6788",
+	"http.log.enable":    true,
+	"http.api.prefix":    "",
+	"redis.addr":         "localhost:6379",
+	"redis.password":     "",
+	"redis.db":           0,
+	"limit.redis.memory": 1 << 30,
+	"limit.queue.num":    10000,
 }
