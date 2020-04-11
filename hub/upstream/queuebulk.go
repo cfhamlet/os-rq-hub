5bb02e45da3de5c10063dc536eec61ba8d360766
Ozzy <cfhamlet@gmail.com>
2020-04-11 23:54:38 +0800

    fix: dead lock

--- hub/upstream/queuebulk.go
+++ hub/upstream/queuebulk.go
@@ -10,17 +10,22 @@ import (
 	"github.com/cfhamlet/os-rq-pod/pod/global"
 )
 
-// QueueUpstreamsPack TODO
-type QueueUpstreamsPack struct {
+// Pack TODO
+type Pack struct {
 	sth.QueueID
-	*slicemap.Map
+	*slicemap.Viewer
+}
+
+// NewPack TODO
+func NewPack(qid sth.QueueID) *Pack {
+	return &Pack{qid, slicemap.NewViewer(nil)}
 }
 
 // QueueBulk TODO
 type QueueBulk struct {
-	mgr  *Manager
-	bulk []*slicemap.Viewer
-	size int64
+	mgr   *Manager
+	bulks []*slicemap.Viewer
+	size  int64
 }
 
 // NewQueueBulk TODO
@@ -37,13 +42,17 @@ func (qb *QueueBulk) Size() int64 {
 	return atomic.LoadInt64(&qb.size)
 }
 
+func (qb *QueueBulk) bulk(id uint64) *slicemap.Viewer {
+	return qb.bulks[qb.index(id)]
+}
+
 func (qb *QueueBulk) index(id uint64) int {
-	return int(id>>32) % len(qb.bulk)
+	return int(id>>32) % len(qb.bulks)
 }
 
 // GetOrAdd TODO
 func (qb *QueueBulk) GetOrAdd(id uint64, f func(slicemap.Item) slicemap.Item) bool {
-	r := qb.bulk[qb.index(id)].GetOrAdd(id, f)
+	r := qb.bulk(id).GetOrAdd(id, f)
 	if r {
 		atomic.AddInt64(&qb.size, 1)
 	}
@@ -52,24 +61,104 @@ func (qb *QueueBulk) GetOrAdd(id uint64, f func(slicemap.Item) slicemap.Item) bo
 
 // GetAndDelete TODO
 func (qb *QueueBulk) GetAndDelete(id uint64, f func(slicemap.Item) bool) bool {
-	r := qb.bulk[qb.index(id)].GetAndDelete(id, f)
+	r := qb.bulk(id).GetAndDelete(id, f)
 	if r {
 		atomic.AddInt64(&qb.size, -1)
 	}
 	return r
 }
 
+// UpdateUpstream TODO
+func (qb *QueueBulk) UpdateUpstream(upstream *Upstream, qMeta *QueueMeta) (int, int) {
+	uNew := 0
+	gNew := 0
+	iid := qMeta.ID.ItemID()
+	qb.GetOrAdd(iid,
+		func(item slicemap.Item) slicemap.Item {
+			var pack *Pack
+			if item == nil {
+				pack = NewPack(qMeta.ID)
+				gNew = 1
+			}
+			pack.GetOrAdd(upstream.ItemID(),
+				func(uitem slicemap.Item) slicemap.Item {
+					if uitem == nil {
+						uNew = 1
+					}
+					upstream.UpdateQueue(qMeta)
+					return upstream
+				})
+			return pack
+		},
+	)
+	return uNew, gNew
+}
+
+// ClearUpstream TODO
+func (qb *QueueBulk) ClearUpstream(id ID, queueIDs []sth.QueueID, ts *time.Time) sth.Result {
+	uDel := 0
+	gDel := 0
+	for _, qid := range queueIDs {
+		u, g := qb.clearQueue(qid, id, ts)
+		uDel += u
+		gDel += g
+	}
+	return sth.Result{"qids": queueIDs, "uid": id, "del": uDel, "del_global": gDel}
+}
+
+// ClearQueue TODO
+func (qb *QueueBulk) ClearQueue(qid sth.QueueID, ids []ID, ts *time.Time) sth.Result {
+	uDel := 0
+	gDel := 0
+	for _, id := range ids {
+		u, g := qb.clearQueue(qid, id, ts)
+		uDel += u
+		gDel += g
+	}
+	return sth.Result{"qid": qid, "uids": ids, "del": uDel, "del_global": gDel}
+}
+
+func (qb *QueueBulk) clearQueue(qid sth.QueueID, id ID, ts *time.Time) (int, int) {
+	uDel := 0
+	gDel := 0
+	iid := qid.ItemID()
+	qb.GetAndDelete(iid,
+		func(item slicemap.Item) bool {
+			pack := item.(*Pack)
+			if pack.Size() <= 0 {
+				gDel++
+				return true
+			}
+			uid := id.ItemID()
+			uitem := pack.Get(uid)
+			if uitem != nil {
+				upstream := uitem.(*Upstream)
+				if upstream.DeleteQueue(qid, ts) {
+					pack.Delete(uid)
+					uDel++
+				}
+			}
+			if pack.Size() <= 0 {
+				gDel++
+				return true
+			}
+			return false
+		},
+	)
+	return uDel, gDel
+}
+
 // PopRequest TODO
 func (qb *QueueBulk) PopRequest(qid sth.QueueID) (req *request.Request, err error) {
 	iid := qid.ItemID()
 	toBeDeleted := make([]ID, 0)
-	qb.bulk[qb.index(iid)].View(iid,
+	qb.bulk(iid).View(iid,
 		func(item slicemap.Item) {
 			if item == nil {
 				err = global.NotExistError(qid.String())
 				return
 			}
-			pack := item.(*QueueUpstreamsPack)
+			pack := item.(*Pack)
 			l := pack.Size()
 			iter := slicemap.NewCycleIter(pack.Map, qb.mgr.rand.Intn(l))
 			iter.Iter(
@@ -87,7 +176,8 @@ func (qb *QueueBulk) PopRequest(qid sth.QueueID) (req *request.Request, err erro
 		},
 	)
 	if len(toBeDeleted) > 0 {
-		go qb.mgr.DeleteOutdated(qid, toBeDeleted, time.Now())
+		ts := time.Now()
+		go qb.ClearQueue(qid, toBeDeleted, &ts)
 	}
 	return
 }
