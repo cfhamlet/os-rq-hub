51450fb8412ca8fdba519399c41adb0f5f206ac8
Ozzy <cfhamlet@gmail.com>
2020-04-15 21:45:29 +0800

    fix: Top

--- hub/upstream/qheap_test.go
+++ hub/upstream/qheap_test.go
@@ -35,3 +35,15 @@ func Test002(t *testing.T) {
 	}
 	assert.Equal(t, hp.Delete(s[3]).ID.Host, s[3].ID.Host)
 }
+
+func Test003(t *testing.T) {
+	hp := upstream.NewQueueHeap()
+	s := []*upstream.Queue{}
+	for i := 0; i < 10; i++ {
+		queue := upstream.NewQueue(nil,
+			upstream.NewQueueMeta(sth.QueueID{Host: fmt.Sprintf("%d", i), Port: "", Scheme: ""}, 0))
+		hp.Push(queue)
+		s = append(s, queue)
+	}
+	assert.Equal(t, s[0], hp.Top())
+}
